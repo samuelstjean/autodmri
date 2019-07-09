a1e4d9acbc6e3d5178cca8ad4151f314253d906d
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2019-07-09 14:37:33 +0200

    add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autodmri',
+    version='0.1',
+    author='Samuel St-Jean',
+    author_email='samuel@isi.uu.nl',
+    packages=find_packages(),
+    scripts=['scripts/get_distribution'],
+    url='https://github.com/samuelstjean/autodmri',
+    license='LICENSE',
+    description='Implementation of "Automated characterization of noise distributions in diffusion MRI data".',
+    long_description=open('README.md').read(),
+    install_requires=['numpy>=1.10',
+                      'scipy>=0.19'],
+)
