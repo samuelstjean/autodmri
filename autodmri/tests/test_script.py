18d402d78b6be5a8828a590942e3be760e5cfdf2
Samuel St-Jean <3030760+samuelstjean@users.noreply.github.com>
2024-01-22 12:26:47 -0500

    revamp script test

--- autodmri/tests/test_script.py
+++ autodmri/tests/test_script.py
@@ -1,9 +1,9 @@
-import os.path
 import subprocess
-
-import autodmri
 import pytest
 
+from pathlib import Path
+
+cwd = Path(__file__).parents[2] / Path("datasets")
 commands = ['autodmri_get_distribution data_SENSE3_MB3_noisemap.nii.gz sigma_maxlk_nmaps.nii.gz N_maxlk_nmaps.nii.gz mask_maxlk_nmaps.nii.gz -m maxlk --noise_maps',
             'autodmri_get_distribution data_SENSE3_MB3_noisemap.nii.gz sigma_nmaps.nii.gz N_nmaps.nii.gz mask_nmaps.nii.gz --noise_maps',
             'autodmri_get_distribution data_SENSE3_MB3_noisemap.nii.gz sigma_nmaps.nii.gz N_nmaps.nii.gz mask_nmaps.nii.gz --noise_maps -f --subsample',
@@ -11,8 +11,6 @@ commands = ['autodmri_get_distribution data_SENSE3_MB3_noisemap.nii.gz sigma_max
             'autodmri_get_distribution dwi_1_8.nii.gz sigma.nii.gz N.nii.gz mask.nii.gz -v',
             'autodmri_get_distribution dwi_1_8.nii.gz sigma.nii.gz N.nii.gz mask.nii.gz -m maxlk -f --ncores 4',
             'autodmri_get_distribution dwi_1_8.nii.gz sigma_maxlk.nii.gz N_maxlk.nii.gz mask_maxlk.nii.gz -m maxlk --size 3 -f -v --axis 0']
-
-cwd = os.path.join(os.path.dirname(autodmri.__file__), '../datasets')
 
 @pytest.mark.parametrize('command', commands)
 def test_script(command):
